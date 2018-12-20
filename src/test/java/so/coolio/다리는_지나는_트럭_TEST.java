eb8e2435db3a1cfb6609667ecdb02d7bfb03ad75
YOUNGMINJUN <youngmin.jun@gmail.com>
2018-12-20 17:15:12 +0900

    - 문제에 대한 주석 추가

--- src/test/java/so/coolio/다리는_지나는_트럭_TEST.java
+++ src/test/java/so/coolio/다리는_지나는_트럭_TEST.java
@@ -8,16 +8,20 @@ import java.util.LinkedList;
 public class 다리는_지나는_트럭_TEST {
     @Test
     public void 다리를_지나는_트럭(){
+        // 지나갈 트럭
         int[] truck_weights = {7, 4, 5, 6};
+        // 다리 길이
         int bridge_length = 2;
 
-        Queue<Integer> tmp_bridge_trucks = new LinkedList<Integer>();
         // 다리 길이만큼 초기화
+        Queue<Integer> tmp_bridge_trucks = new LinkedList<Integer>();
         for(int i=0; i < bridge_length; i++){
             tmp_bridge_trucks.offer(0);
         }
 
+        // 결과 시간
         int answer = 0;
+        // 다리의 무게
         int weight = 10;
 
         for(int truck : truck_weights){
@@ -36,6 +40,7 @@ public class 다리는_지나는_트럭_TEST {
             tmp_bridge_trucks.offer(new Integer(truck));
         }
 
+        // 다리에 남아 있는 트럭이 있다면 모두 이동
         while(!bridegeEmptyCheck(tmp_bridge_trucks)){
             answer += 1;
             tmp_bridge_trucks.poll();
@@ -46,6 +51,7 @@ public class 다리는_지나는_트럭_TEST {
     }
 
     /**
+     * 생성된 길이 만큼 Queue를 생성하고 값을 0으로 채워서 Quque의 값이 들어 있는지를 확인
      * 비어 있으면 TRUE, 값이 있으면 FALSE
      * @param tmp_bridge_trucks
      * @return
@@ -65,6 +71,7 @@ public class 다리는_지나는_트럭_TEST {
     }
 
     /**
+     * 입력되는 값을 Queue에 넣어서 크기를 넘는지 안넘는지 확인
      * 들어간 Queue 값이 문제 없으면 True, 문제 있으면 False
      * @param weight
      * @param tmp_bridge_trucks
