35dfb14605cae803edecafe437c8b2e509bf45dc
youngminjun <youngmin.jun@gmail.com>
2019-08-30 10:40:39 +0900

    [힙] 더 맵게 문제 추가
    값을 자동으로 오름차순으로 정렬해주는 PriorityQueue를 활용하여 문제해결

---
+++ src/test/java/so/coolio/heap/더_맵게_TEST.java
@@ -0,0 +1,69 @@
+package so.coolio.heap;
+
+import java.util.PriorityQueue;
+
+import org.junit.Test;
+
+/**
+문제 설명
+매운 것을 좋아하는 Leo는 모든 음식의 스코빌 지수를 K 이상으로 만들고 싶습니다. 모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 Leo는 스코빌 지수가 가장 낮은 두 개의 음식을 아래와 같이 특별한 방법으로 섞어 새로운 음식을 만듭니다.
+
+섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
+Leo는 모든 음식의 스코빌 지수가 K 이상이 될 때까지 반복하여 섞습니다.
+Leo가 가진 음식의 스코빌 지수를 담은 배열 scoville과 원하는 스코빌 지수 K가 주어질 때, 모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 섞어야 하는 최소 횟수를 return 하도록 solution 함수를 작성해주세요.
+
+제한 사항
+scoville의 길이는 1 이상 1,000,000 이하입니다.
+K는 0 이상 1,000,000,000 이하입니다.
+scoville의 원소는 각각 0 이상 1,000,000 이하입니다.
+모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우에는 -1을 return 합니다.
+입출력 예
+scoville	K	return
+[1, 2, 3, 9, 10, 12]	7	2
+입출력 예 설명
+스코빌 지수가 1인 음식과 2인 음식을 섞으면 음식의 스코빌 지수가 아래와 같이 됩니다.
+새로운 음식의 스코빌 지수 = 1 + (2 * 2) = 5
+가진 음식의 스코빌 지수 = [5, 3, 9, 10, 12]
+
+스코빌 지수가 3인 음식과 5인 음식을 섞으면 음식의 스코빌 지수가 아래와 같이 됩니다.
+새로운 음식의 스코빌 지수 = 3 + (5 * 2) = 13
+가진 음식의 스코빌 지수 = [13, 9, 10, 12]
+
+모든 음식의 스코빌 지수가 7 이상이 되었고 이때 섞은 횟수는 2회입니다.
+ */
+public class 더_맵게_TEST {
+
+	@Test
+	public void test() {
+		int[] scoville = {0,0,1};
+		int K = 7;
+		
+		System.err.println(solution(scoville, K));
+	}
+	
+	public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> sources = new PriorityQueue<>();
+        for(int source : scoville){
+        	sources.add(source);
+        }
+        
+        
+        while (true) {
+        	// 가장 작은 값이 K이상이면 종료
+        	if(sources.peek() >= K) break;
+        	// 소스의 사이즈가 1이면 종료 : 연산을 위해서는 취소 2개이상이 필요함
+        	if(sources.size() == 1) return -1;
+        	
+        	answer++;
+        	int firstSource = sources.poll();
+        	int secondSource = sources.poll();
+        	
+        	int mixSource = firstSource + (secondSource * 2);
+        	sources.add(mixSource);
+		}
+        
+        return answer;
+    }
+
+}
