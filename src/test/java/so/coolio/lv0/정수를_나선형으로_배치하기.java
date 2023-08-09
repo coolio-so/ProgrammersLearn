786b48a7fb0c47ef94031871f5125580710d5552
CoolioSo <CoolioSo@172.16.150.64>
2023-08-09 09:32:55 +0900

    - 정수를 나선형으로 배치

--- src/test/java/so/coolio/lv0/정수를_나선형으로_배치하기.java
+++ src/test/java/so/coolio/lv0/정수를_나선형으로_배치하기.java
@@ -3,6 +3,9 @@
  */
 package so.coolio.lv0;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 /**
  * <p>
  *
@@ -14,39 +17,70 @@ package so.coolio.lv0;
  */
 public class 정수를_나선형으로_배치하기 {
 	public static void main(String[] args) {
-		System.err.println(solution(4));
+		Integer[][] result = solution(5);
+
+		Stream.of(result)
+			  .flatMap(item -> Arrays.stream(item))
+			  .peek(System.err::println)
+			  .forEach(System.out::println);
+
 	}
 
-	public static int[][] solution(int n) {
-		int[][] answer = new int[n][n];
+	public static Integer[][] solution(int n) {
+		Integer[][] answer = new Integer[n][n];
 		int minColumn = 0;
 		int maxColumn = n - 1;
 		int minRow = 0;
-		int maxRow = n = 1;
+		int maxRow = n - 1;
 		int direction = 0;
 		int roate = 0;
 		int currentValue = 0;
 
-		while (currentValue < n * n) {
-			answer[minColumn][currentValue] = ++currentValue;
+		int column = 0; int row = 0;
 
-			switch (direction) {
-			case 0:
+		while (currentValue <= (n * n)) {
+			answer[row][column] = ++currentValue;
+
+			if(direction == 0) {
 				System.err.println("오른쪽");
-				break;
-			case 1:
-				System.err.println("하단");
-				break;
-			case 2:
-				System.err.println("왼쪽");
-				break;
-			case 3:
-				System.err.println("상단");
-				break;
+				if(column == (maxColumn - roate)) {
+					direction = 1;
+					row++;
+				}else {
+					column++;
+				}
 
-			default:
-				break;
+			}else if(direction == 1) {
+				System.err.println("하단");
+				if(row == (maxRow - roate)) {
+					direction = 2;
+					column--;
+				}else {
+					row++;
+				}
+
+			}else if(direction == 2) {
+				System.err.println("왼쪽");
+				if(column == (minColumn + roate)) {
+					direction = 3;
+					row--;
+					roate++;
+				}else {
+					column--;
+				}
+
+			}else if(direction == 3) {
+				System.err.println("상단");
+				if(row == (minRow + roate)) {
+					direction = 0;
+					column++;
+				}else {
+					row--;
+				}
 			}
+
+			System.err.println(column + ", " + row + " = " + answer[column][row] );
+			System.err.println(currentValue);
 		}
 
 		return answer;
